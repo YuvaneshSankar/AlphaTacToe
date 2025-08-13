01980a22c57d7055a5c125acff547b5813d18560
YuvaneshSankar <yuvanesh.skv@gmail.com>
2025-08-13 13:30:07 +0530

    Started value iteration

---
+++ valueIteration.py
@@ -0,0 +1,5 @@
+#the process do mtcs for every state
+#then start findin pie values considering each state as a root state
+#then after start the simulation by playing one game at time
+#then start the back propagation of the DNN and update to get better p and v values
+#now the monte carlo tree search is done
